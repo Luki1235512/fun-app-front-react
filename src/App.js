c1b0f9b5cfa5e5c6c9bcbf9d2422729f62bd4bce
Luki1235512 <Luki1235512@gmail.com>
2022-02-09 18:49:31 +0100

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
