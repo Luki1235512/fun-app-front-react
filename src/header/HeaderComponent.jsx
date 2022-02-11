e9bb887fb8ad703fd330efffbecd4d855754e709
Luki1235512 <Luki1235512@gmail.com>
2022-02-12 00:04:43 +0100

    changed urls

--- src/header/HeaderComponent.jsx
+++ src/header/HeaderComponent.jsx
@@ -13,12 +13,14 @@ class HeaderComponent extends Component {
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                         <div>
-                            <a href="https://fun-app-19ltju.herokuapp.com" className="navbar-brand">Fun App</a>
+                            {/*<a href="https://fun-app-19ltju.herokuapp.com" className="navbar-brand">Fun App</a>*/}
+                            <a href="https://fun-app-react-develop.herokuapp.com" className="navbar-brand">Fun App</a>
                             | &nbsp;
+                            {/*<a href="https://fun-app-19ltju.herokuapp.com/employees" className="navbar-brand">Employees</a>*/}
                             <a href="https://fun-app-19ltju.herokuapp.com/employees" className="navbar-brand">Employees</a>
                             | &nbsp;
-                            <a href="https://fun-app-19ltju.herokuapp.com/game" className="navbar-brand">Game</a>
-                            {/*<a href="http://localhost:3000/game" className="navbar-brand">Game</a>*/}
+                            {/*<a href="https://fun-app-19ltju.herokuapp.com/game" className="navbar-brand">Game</a>*/}
+                            <a href="https://fun-app-react-develop.herokuapp.com/game" className="navbar-brand">Game</a>
                         </div>
                     </nav>
                 </header>
