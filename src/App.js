87684f423b9ddfe4a23815a37fc449bf0e3e8661
Luki1235512 <Luki1235512@gmail.com>
2022-02-16 14:38:12 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+// import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import ListEmployeeComponent from "./employee/components/ListEmployeeComponent";
 import HeaderComponent from "./header/HeaderComponent";
 import FooterComponent from "./header/FooterComponent";
 import CreateEmployeeComponent from "./employee/components/CreateEmployeeComponent";
 import UpdateEmployeeComponent from "./employee/components/UpdateEmployeeComponent";
 import ViewEmployeeComponent from "./employee/components/ViewEmployeeComponent";
-// import {Canvas} from "@react-three/fiber";
+import mainComponent from "./game/main";
+import { Route } from "wouter";
 
 
 function App() {
   return (
       <div>
-          <Router>
-              <HeaderComponent />
-              <div className="container">
-                  <Switch>
-                      <Route path = "/" exact />
-                      <Route path = "/employees" component = {ListEmployeeComponent}/>
-                      <Route path = "/add-employee" component = {CreateEmployeeComponent}/>
-                      <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}/>
-                      <Route path = "/view-employee/:id" component = {ViewEmployeeComponent}/>
-                      {/*<Route path = "/game" component = {mainComponent}/>*/}
-                  </Switch>
-              </div>
-              <FooterComponent />
-          </Router>
+          <Route path="/game" component={mainComponent} />
+          {/*<Router>*/}
+          {/*    <HeaderComponent />*/}
+          {/*    <div className="container">*/}
+          {/*        <Switch>*/}
+          {/*            <Route path = "/" exact />*/}
+          {/*            <Route path = "/employees" component = {ListEmployeeComponent}/>*/}
+          {/*            <Route path = "/add-employee" component = {CreateEmployeeComponent}/>*/}
+          {/*            <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}/>*/}
+          {/*            <Route path = "/view-employee/:id" component = {ViewEmployeeComponent}/>*/}
+          {/*            <Route path = "/game" component = {mainComponent}/>*/}
+          {/*        </Switch>*/}
+          {/*    </div>*/}
+          {/*    <FooterComponent />*/}
+          {/*</Router>*/}
       </div>
   );
 }
