94884d91313ea0b875da11d8f2597185df2cb7a9
Luki1235512 <Luki1235512@gmail.com>
2022-02-21 10:02:25 +0100

    removed game and packages

--- src/header/HeaderComponent.jsx
+++ src/header/HeaderComponent.jsx
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 
+const master = "https://fun-app-19ltju.herokuapp.com"
+const local = "http://localhost:3000"
+
 class HeaderComponent extends Component {
     constructor(props) {
         super(props)
@@ -13,9 +16,11 @@ class HeaderComponent extends Component {
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                         <div>
-                            <a href="https://fun-app-19ltju.herokuapp.com" className="navbar-brand">Fun App</a>
+                            {/*<a href="https://fun-app-19ltju.herokuapp.com" className="navbar-brand">Fun App</a>*/}
+                            <a href={local} className="navbar-brand">Fun App</a>
                             | &nbsp;
-                            <a href="https://fun-app-19ltju.herokuapp.com/employees" className="navbar-brand">Employees</a>
+                            {/*<a href="https://fun-app-19ltju.herokuapp.com/employees" className="navbar-brand">Employees</a>*/}
+                            <a href={local + "/employees"} className="navbar-brand">Employees</a>
                             | &nbsp;
                             {/*<a href="http://localhost:3000/game" className="navbar-brand">Game</a>*/}
                         </div>
