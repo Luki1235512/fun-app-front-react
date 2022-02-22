9d98e8a8bd3ef11b86eb1b495e8e00416f2e4100
Luki1235512 <Luki1235512@gmail.com>
2022-02-22 14:55:42 +0100

    created game component

---
+++ src/game/main.js
@@ -0,0 +1,13 @@
+
+
+const main = () => {
+    return (
+        <div className="game-container">
+            <canvas className="game-canvas" width="352" height="198">
+
+            </canvas>
+        </div>
+    )
+}
+
+export default main()
